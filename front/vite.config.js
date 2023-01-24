015d8ca17980d32adaa3487363943cb68421752e
admingiant <artemgiant@gmail.com>
2023-01-24 18:03:24 +0200

    Копирования файлов

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/js',
+          dest: 'assets'
+        }
+      ]
+    })
+  ],
   base: "/",
   resolve: {
     alias: {
