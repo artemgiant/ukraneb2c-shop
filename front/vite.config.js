00fa660903948a918af9a8059e227b4b60b2a9a8
admingiant <artemgiant@gmail.com>
2023-01-24 18:44:56 +0200

    Изключения из обработки js

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
