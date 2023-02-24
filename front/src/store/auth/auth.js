f663280a0b4f75cd50e38eae92c36a1355447e74
admingiant <artemgiant@gmail.com>
2023-02-24 19:37:03 +0200

    Коментариия

--- front/src/store/auth/auth.js
+++ front/src/store/auth/auth.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8001";
+axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
