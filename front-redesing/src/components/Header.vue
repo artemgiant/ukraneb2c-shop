2d61ce1874822b7df916e638e24ded787a4c5d8f
admingiant <artemgiant@gmail.com>
2023-06-07 17:39:28 +0300

    Переклад

--- front-redesing/src/components/Header.vue
+++ front-redesing/src/components/Header.vue
@@ -100,7 +100,7 @@
       <form class="navbar-search" style="width: 80%" method="get" action="/">
         <label class="sr-only screen-reader-text" for="search">Search for:</label>
         <div class="input-group">
-          <input type="text" id="search" class="form-control search-field" dir="ltr"  v-model="productStore.filters.search"  placeholder="Search for products">
+          <input type="text" id="search" class="form-control search-field" dir="ltr"  v-model="productStore.filters.search"  placeholder="Пошук по назві товару...">
           <div class="input-group-btn">
             <input type="hidden" id="search-param" name="post_type" value="product">
             <button type="submit" class="btn btn-secondary"><i class="ec ec-search"></i></button>
